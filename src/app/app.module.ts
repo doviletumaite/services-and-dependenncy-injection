c372e7fd2f3bd0b1d517cf74a36a9f7ed44d6a25
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-30 16:32:16 +0200

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CounterService } from './counter.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     NgbModule
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
