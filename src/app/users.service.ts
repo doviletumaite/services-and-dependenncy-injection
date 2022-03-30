66a9a0b339d2f2a5c07dd2aa33ee08c1b1c8e174
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-30 15:28:34 +0200

    Update users.service.ts

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -3,10 +3,12 @@ export class UserService {
   inactiveUsers = ['Chris', 'Manu']
 
   setToActive(id: number){
-
+    this.activeUsers.push(this.inactiveUsers[id])
+    this.inactiveUsers.splice(id, 1)
   }
 
   setToInactive(id: number){
-
+    this.inactiveUsers.push(this.inactiveUsers[id])
+    this.activeUsers.splice(id, 1)
   }
 }
